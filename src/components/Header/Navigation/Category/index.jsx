d1f4d5e4df2c67c873bfcf33829af0a64628246b
ViktorGubarevich <gubarew.v@gmail.com>
2023-08-01 18:27:44 +0300

    Added Category.

--- src/components/Header/Navigation/Category/index.jsx
+++ src/components/Header/Navigation/Category/index.jsx
@@ -1,3 +1,28 @@
-const Category = ({ list }) => <div className="category">Category</div>;
+import { NavLink, useLocation } from "react-router-dom";
+import style from "./Category.module.scss";
+import classNames from "classnames";
+
+const Category = ({ list }) => {
+  const location = useLocation();
+  const gender = location.pathname.split("/")[1] || "women";
+  const categoriesList = list.find((item) => item.link === gender);
+
+  return (
+    <ul className={style.category}>
+      {categoriesList.categories.map((item) => (
+        <li key={item.link} className={style.item}>
+          <NavLink
+            to={`${gender}/${item.link}`}
+            className={({ isActive }) =>
+              classNames(style.link, isActive && style.linkActive)
+            }
+          >
+            {item.title}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Category;
--- src/components/Header/Navigation/Gender/index.jsx
+++ src/components/Header/Navigation/Gender/index.jsx
@@ -1,22 +1,30 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import style from "./Gender.module.scss";
 import classNames from "classnames";
 
-const Gender = ({ list }) => (
-  <ul className={style.gender}>
-    {list.map((item) => (
-      <li key={item.link} className={style.item}>
-        <NavLink
-          to={item.link}
-          className={({ isActive }) =>
-            classNames(style.link, isActive && style.linkActive)
-          }
-        >
-          {item.title}
-        </NavLink>
-      </li>
-    ))}
-  </ul>
-);
+const Gender = ({ list }) => {
+  const location = useLocation();
+  const gender = location.pathname.split("/")[1] || "women";
+
+  return (
+    <ul className={style.gender}>
+      {list.map((item) => (
+        <li key={item.link} className={style.item}>
+          <NavLink
+            to={item.link}
+            className={({ isActive }) =>
+              classNames(
+                style.link,
+                (isActive || gender === item.link) && style.linkActive
+              )
+            }
+          >
+            {item.title}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Gender;
